619cf295083c77ac7342878bee7cd985333538f1
Omar Trejo <omar@omartrejo.com>
2020-03-16 03:05:33 -0600

    Add tracking of events/questions for Google Analytics

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -4,3 +4,34 @@ function gtag() {
 }
 gtag("js", new Date());
 gtag("config", "UA-98004849-1");
+
+let questions = {
+  headingOne: "call",
+  headingTwo: "process",
+  headingThree: "price",
+  headingFour: "retainers",
+  headingFive: "on-site",
+  headingSix: "expertise",
+  headingSeven: "training",
+  headingEight: "send-me-work",
+  headingNine: "work-for-you",
+  headingTen: "current-work"
+};
+
+for (const q in questions) {
+  $(`#${q}`).click(function() {
+    if (
+      $(this)
+        .parent()
+        .attr("class")
+        .split(/\s+/)
+        .includes("collapsed")
+    ) {
+      if (location.hostname == "localhost") {
+        console.log(`Would send "questions/${questions[q]}" to GA`);
+      } else {
+        ga("send", "event", "questions", questions[q]);
+      }
+    }
+  });
+}
